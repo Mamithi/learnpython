0cd09f604c1d61a7e3acc1466c327d94223d11f6
Mamithi <19lawrence93@gmail.com>
2019-04-28 23:00:10 +0300

    Create master template

--- django/myproject/myproject/boards/views.py
+++ django/myproject/myproject/boards/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Board
 
 # Create your views here.
+
+
 def home(request):
     boards = Board.objects.all()
-    
+
     return render(request, 'home.html', {'boards': boards})
 
-def board_topics(request, pk):
-    try:
-         board = Board.objects.get(pk=pk)
-    except Board.DoesNotExist:
-        raise Http404
 
-    return render(request, 'topics.html', {'board':board})
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    return render(request, 'topics.html', {'board': board})
