eb42f92eb5684651911e11b6ce7868e630aba593
Mamithi <19lawrence93@gmail.com>
2019-04-28 11:50:20 +0300

    Create django first app

---
+++ django/firstapp/django2.2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lawrence/Projects/learnpython/django/firstapp/django2.2/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
