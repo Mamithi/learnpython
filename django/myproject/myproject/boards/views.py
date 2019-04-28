f672d81b6ae303ef983a7d1b06beebac4cebbc0c
Mamithi <19lawrence93@gmail.com>
2019-04-28 21:43:25 +0300

    Advanced urls

--- django/myproject/myproject/boards/views.py
+++ django/myproject/myproject/boards/views.py
@@ -7,3 +7,7 @@ def home(request):
     boards = Board.objects.all()
     
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board':board})
--- django/myproject/myproject/myproject/urls.py
+++ django/myproject/myproject/myproject/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     path('admin/', admin.site.urls),
 ]
