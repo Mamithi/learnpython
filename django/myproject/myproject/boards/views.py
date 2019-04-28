836e1e7053e059d35e41413597eca467902dc124
Mamithi <19lawrence93@gmail.com>
2019-04-28 23:17:15 +0300

    forms

--- django/myproject/myproject/boards/views.py
+++ django/myproject/myproject/boards/views.py
@@ -15,3 +15,7 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
 
     return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk)
+    return render(request, 'new_topic.html', {'board': board})
--- django/myproject/myproject/myproject/urls.py
+++ django/myproject/myproject/myproject/urls.py
@@ -20,5 +20,6 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]
