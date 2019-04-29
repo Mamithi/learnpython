71ebf1b1b3119f8280bc82e54be0afcbf6fafc70
Mamithi <19lawrence93@gmail.com>
2019-04-29 15:17:57 +0300

    Reausable forms

--- django/myproject/myproject/boards/views.py
+++ django/myproject/myproject/boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from .models import Board
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 # Create your views here.
 
@@ -13,9 +14,29 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    
 
     return render(request, 'topics.html', {'board': board})
 
 def new_topic(request, pk):
-    board = get_object_or_404(Board, pk)
-    return render(request, 'new_topic.html', {'board': board})
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit = False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message = form.cleaned_data.get('message'),
+                topic = topic,
+                created_by = user                
+            )
+
+            return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
