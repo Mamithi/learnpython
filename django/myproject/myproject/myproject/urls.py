e427fe92232226fb2b7692333f62522a5ebca0a9
Mamithi <19lawrence93@gmail.com>
2019-04-30 09:45:04 +0300

    LOgout

--- django/myproject/myproject/myproject/urls.py
+++ django/myproject/myproject/myproject/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from boards import views
+from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
