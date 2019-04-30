40f9eecf8fe1410a7f16edfea01b138526cdc9b4
Mamithi <19lawrence93@gmail.com>
2019-04-30 15:12:13 +0300

    Auth

--- django/myproject/myproject/myproject/urls.py
+++ django/myproject/myproject/myproject/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
