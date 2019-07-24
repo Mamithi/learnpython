1b56fd3644d186b8b96a934a72ed2dfb8e2788e5
Mamithi <lawmamithi@gmail.com>
2019-07-24 17:09:05 +0300

    Start of style transfer

---
+++ neural_networks/deep_learning/pytorch-lessons/lesson6/style_transfer.py
@@ -0,0 +1,11 @@
+from PIL import Image
+from io import BytesIO 
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+import torch 
+import torch.optim as optim 
+import requests 
+from torchvision import transformsm, models 
+
+print("Hello")
